30cdc2333d08340d8cafac3eb64c4207995a420b
Seohyun <tjgus9138@naver.com>
2025-03-06 16:18:14 +0900

    [0306] SpringBoot

--- SpringBootTest/src/main/java/com/uplus/ureka/book/controller/BookRestController.java
+++ SpringBootTest/src/main/java/com/uplus/ureka/book/controller/BookRestController.java
@@ -11,14 +11,16 @@ import com.uplus.ureka.book.model.dto.BookException;
 import com.uplus.ureka.book.model.dto.PageBean;
 import com.uplus.ureka.book.model.service.BookService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
- * @RestController  
- *   RestFul Service를 위한 Conrtoller 
+ * @RestController
+ *   RestFul Service를 위한 Conrtoller
  *   모든 메서드의 응답을  @ResponseBody를 붙여주는 효과
  */
-@RestController	
+@RestController
 
 //RestFul에서 서비스할 자원(Domain)명을 붙인다
 @RequestMapping("/book")
@@ -26,9 +28,9 @@ import java.util.List;
 /*
  * @CrossOrigin
  *  - CORS 요청에 대한 승인
- *  origins = {"*"}  : 요청하는 모든 URL, 메서드를 승인 
+ *  origins = {"*"}  : 요청하는 모든 URL, 메서드를 승인
  *     ==> 보안에 취약하므로 상용에서는 사용 안함
- *     ==> 이후에 Configuration을 통해 설정할 예정  
+ *     ==> 이후에 Configuration을 통해 설정할 예정
  * */
 
 @CrossOrigin(origins = {"*"})
@@ -53,19 +55,32 @@ public class BookRestController {
 	 * ResponseEntity<?>
 	 * ? : anyType
 	 */
-//
-//	@GetMapping
-//	public ResponseEntity<?> searchAll(PageBean bean) {
-//
-//	}
+
+	@GetMapping
+	public ResponseEntity<?> searchAll(PageBean bean) {
+		logger.debug("bean: {}", bean);
+		List<Book> books = service.searchAll(bean);
+		logger.debug("books: {}", books);
+
+		if(books!=null && books.size() > 0) {
+			Map<String, Object> result = new HashMap<>();
+
+			result.put("books", books);
+			result.put("page", bean);
+
+			return new ResponseEntity<Map>(result, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		}
+	}
 
 	/**
-	 * error 메세지 처리시 한글인 경우 깨지므로 한글 처리를 해야한다. 
+	 * error 메세지 처리시 한글인 경우 깨지므로 한글 처리를 해야한다.
 	 * Content-Type : application/json;charset-UTF-8
 	 * @ExceptionHandler
 	 * 해당 컨트롤러에서 발생하는 오류를 처리하는 Annotation
 	 */
-	
+
 	/**
 	 * ResponseEntity
 	 * 응답 데이타를 담을 객체
@@ -73,18 +88,18 @@ public class BookRestController {
 
 	/**
 	 * @PathVariable
-	 * 요청 데이타가 url에 있는 경우 path에서 데이타를 전달 받을 때 사용하는 Annotation 
-	 * Get 방식에서 범위 데이타를 요청할때와 pk를 이용한 1개의 데이타를 요청할때를 
-	 * 구별하기 path로 전달된 pk를 추출할때 사용한다. 
+	 * 요청 데이타가 url에 있는 경우 path에서 데이타를 전달 받을 때 사용하는 Annotation
+	 * Get 방식에서 범위 데이타를 요청할때와 pk를 이용한 1개의 데이타를 요청할때를
+	 * 구별하기 path로 전달된 pk를 추출할때 사용한다.
 	 */
 
-	
+
 	/**
 	 * @RequestBody
-	 * 요청 방식이 Put과 Post이면서 요청 데이타가 JOSON 형식일 때 
-	 * 전달되는 요청 packet의 body를 객체로 전달 받을 때 사용하는 Annotation 
+	 * 요청 방식이 Put과 Post이면서 요청 데이타가 JOSON 형식일 때
+	 * 전달되는 요청 packet의 body를 객체로 전달 받을 때 사용하는 Annotation
 	 */
-  
+
 
 }
 
