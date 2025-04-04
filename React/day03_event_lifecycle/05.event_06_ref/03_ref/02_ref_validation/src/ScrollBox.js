ea25b19b328a0754e8e4a048efc3a9efc0c7983e
seohyunn2 <tjgus9138@naver.com>
2025-04-04 09:07:25 +0900

    [0403] React

--- React/day03_event_lifecycle/05.event_06_ref/03_ref/02_ref_validation/src/ScrollBox.js
+++ React/day03_event_lifecycle/05.event_06_ref/03_ref/02_ref_validation/src/ScrollBox.js
@@ -1,18 +1,37 @@
-/*
-    overflow 상황에서 
-    clientHeight : 요소 - (스크롤+보더)
-    scrollHeight : 요소 내부의 content 크기
-    scrollTop : content의 맨 처음 부터 현재 화면에 보이기 전까지
-*/
-const style = {
-  border: "1px solid black",
-  height: "300px",
-  width: "300px",
-  overflow: "auto", //요소의 콘텐츠가 요소보다 클때 처리 방식
-  position: "relative",
-};
-const innerStyle = {
-  width: "100%",
-  height: "1000px",
-  background: "linear-gradient(white, black)",
-};
+// /*
+//     overflow 상황에서
+//     clientHeight : 요소 - (스크롤+보더)
+//     scrollHeight : 요소 내부의 content 크기
+//     scrollTop : content의 맨 처음 부터 현재 화면에 보이기 전까지
+// */
+
+// import React, { Component } from "react";
+
+// class ScrollBox extends Component {
+//   ScrollToBottom = () => {
+//     console.log("box", this.box);
+//     const { scrollHeight, cliendHeight } = this.box;
+//     this.box.scrollTop = scrollHeight - cliendHeight;
+//   };
+
+//   render() {
+//     const style = {
+//       border: "1px solid black",
+//       height: "300px",
+//       width: "300px",
+//       overflow: "auto", //요소의 콘텐츠가 요소보다 클때 처리 방식
+//       position: "relative",
+//     };
+//     const innerStyle = {
+//       width: "100%",
+//       height: "1000px",
+//       background: "linear-gradient(white, black)",
+//     };
+
+//     return (
+//       <div style={style}>
+//         <div style={innerStyle}></div>
+//       </div>
+//     );
+//   }
+// }
--- React/day03_event_lifecycle/05.event_06_ref/04_form/src/component/SelectF.js
+++ React/day03_event_lifecycle/05.event_06_ref/04_form/src/component/SelectF.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
 
 function SelectF(props) {
+  const [value, setValue] = useState("mango");
+  const flavors = [
+    { key: "grapefruit", value: "포도" },
+    { key: "lime", value: "라임" },
+    { key: "coconut", value: "코코넛" },
+    { key: "mango", value: "Mango" },
+  ];
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const flavorList = flavors.map((f) => <option value={f.key}>{f.value}</option>);
   return (
     <form>
       <label>
         Pick your favorite flavor:
-        <select></select>
+        <select value={value} onChange={handleChange}>
+          {flavorList}
+        </select>
       </label>
       <input type="submit" value="Submit" />
     </form>
